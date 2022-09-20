c606f02e8faf5bb3efdfd9952a5d02c4bb7a9cf3
Markus Iorio <markus.iorio@centralnic.com>
2022-09-20 07:59:59 +0000

    formating

--- refactor.py
+++ refactor.py
@@ -1,28 +1,26 @@
 import peewee as pw
-from playhouse.reflection import generate_models, print_model
+from playhouse.reflection import generate_models
 
 DB = pw.SqliteDatabase("Chinook_Sqlite_AutoIncrementPKs.sqlite")
 
 
 class SQLiteDataBase:
-    def __init__(self, database:pw.SqliteDatabase) -> None:
+    def __init__(self, database: pw.SqliteDatabase) -> None:
         self.__DB = database
         self.__models = generate_models(self.__DB)
         globals().update(self.__models)
-    
+
     def __str__(self) -> str:
         return "\n".join(list(self.__models))
-    
+
     def __repr__(self) -> str:
         return str(list(self.__models))
-
 
 
 def main():
     sql_db = SQLiteDataBase(DB)
     print(sql_db)
-   
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
